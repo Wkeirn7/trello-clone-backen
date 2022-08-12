366bc9bf4a5c2cc96825fb0da5c41bbe108c4c24
wkeirn7 <wkeirn7@gmail.com>
2022-08-12 14:28:17 -0400

    edited boards urls to support nested routes

--- boards/urls.py
+++ boards/urls.py
@@ -1,11 +1,28 @@
-from django.urls import path
+from cgitb import lookup
+from django.urls import path, include
 from rest_framework import routers
 from .views import UserBoardsViewSet, ListViewSet, CardViewSet
+from rest_framework_nested import routers
 
 router = routers.SimpleRouter()
 
-router.register(r'boards_list', UserBoardsViewSet, basename='boards_list')
-router.register(r'lists', ListViewSet, basename='lists')
-router.register(r'cards', CardViewSet, basename='cards')
+#URL Patterns:
+#   boards/boards_list/
+#   boards/boards_list/{board_pk}
+#   boards/boards_list/{board_pk}/lists/
+#   boards/boards_list/{board_pk}/lists/{list_pk}
+#   boards/boards_list/{board_pk}/lists/{list_pk}/cards/
+#   boards/boards_list/{board_pk}/lists/{list_pk}/cards/{card_pk}/
 
-urlpatterns = router.urls
+router.register(r'boards_list', UserBoardsViewSet, basename='boards_list')
+
+boards_router = routers.NestedSimpleRouter(router, r'boards_list', lookup='boards_list')
+boards_router.register(r'lists', ListViewSet, basename='lists')
+
+list_router = routers.NestedSimpleRouter(boards_router, r'lists', lookup='lists')
+list_router.register(r'cards', CardViewSet, basename='cards')
+
+urlpatterns = [
+    path(r'', include(router.urls)),
+    path(r'', include(boards_router.urls)),
+]
