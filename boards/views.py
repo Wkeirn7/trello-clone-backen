76c96cfeab6a96cb7a89303204e0ecc6482da531
wkeirn7 <wkeirn7@gmail.com>
2022-08-14 13:18:52 -0400

    can CRUD nested lists

--- boards/views.py
+++ boards/views.py
@@ -2,27 +2,51 @@ from rest_framework import viewsets
 from .models import Board, List, Card, Person
 from .serializers import UserBoardSerializer, ListSerializer, CardSerializer, PersonSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 # Create your views here.
 class UserBoardsViewSet(viewsets.ModelViewSet):
     serializer_class = UserBoardSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        return Board.objects.filter(owner=self.request.user)
+    def list(self, request,):
+        queryset = Board.objects.filter(owner=request.user)
+        serializer = UserBoardSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Board.objects.filter(owner=request.user)
+        board = get_object_or_404(queryset, pk=pk)
+        serializer = UserBoardSerializer(board)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
 
 class ListViewSet(viewsets.ModelViewSet):
-    queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [IsAuthenticated]
 
+    def list(self, request, boards_list_pk=None):
+        queryset = List.objects.filter(board=boards_list_pk)
+        serializer = ListSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, boards_list_pk=None):
+        queryset = List.objects.filter(pk=pk, board=boards_list_pk)
+        list_obj = get_object_or_404(queryset, pk=pk)
+        serializer = ListSerializer(list_obj)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
-        board = Board.objects.get(id=self.request.data['board_id'])
-        print(board)
+        print(self.kwargs['boards_list_pk'])
+        board = Board.objects.get(id=self.kwargs['boards_list_pk'])
         return serializer.save(board=board)
+
+    def get_queryset(self):
+        print(self.kwargs)
+        return List.objects.filter(board=self.kwargs['boards_list_pk'], id=self.kwargs['pk'])
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
