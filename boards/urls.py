4a716caad5692e1c0575c09f930ddcbfb84b66cc
wkeirn7 <wkeirn7@gmail.com>
2022-08-19 21:06:58 -0400

    added list, create for card participants

--- boards/urls.py
+++ boards/urls.py
@@ -1,7 +1,6 @@
-from cgitb import lookup
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserBoardsViewSet, ListViewSet, CardViewSet, BoardPersonViewSet
+from .views import UserBoardsViewSet, ListViewSet, CardViewSet, BoardPersonViewSet, CardPersonViewSet
 from rest_framework_nested import routers
 
 router = routers.SimpleRouter()
@@ -9,10 +8,16 @@ router = routers.SimpleRouter()
 #URL Patterns:
 #   boards/boards_list/
 #   boards/boards_list/{board_pk}
+#   boards/boards_list/{board_pk}/participants
+#   boards/boards_list/{board_pk}/participants/{participant_pk}
 #   boards/boards_list/{board_pk}/lists/
 #   boards/boards_list/{board_pk}/lists/{list_pk}
 #   boards/boards_list/{board_pk}/lists/{list_pk}/cards/
 #   boards/boards_list/{board_pk}/lists/{list_pk}/cards/{card_pk}/
+#   boards/boards_list/{board_pk}/lists/{list_pk}/cards/{card_pk}/card_participants
+#   boards/boards_list/{board_pk}/lists/{list_pk}/cards/{card_pk}/card_participants/{card_participant_pk}
+
+
 
 router.register(r'boards_list', UserBoardsViewSet, basename='boards_list')
 
@@ -23,8 +28,12 @@ boards_router.register(r'participants', BoardPersonViewSet, basename='participan
 list_router = routers.NestedSimpleRouter(boards_router, r'lists', lookup='lists')
 list_router.register(r'cards', CardViewSet, basename='cards')
 
+card_router = routers.NestedSimpleRouter(list_router, r'cards', lookup='cards')
+card_router.register(r'card_participants', CardPersonViewSet, basename='card_participants')
+
 urlpatterns = [
     path(r'', include(router.urls)),
     path(r'', include(boards_router.urls)),
     path(r'', include(list_router.urls)),
+    path(r'', include(card_router.urls)),
 ]
--- boards/views.py
+++ boards/views.py
@@ -107,5 +107,43 @@ class BoardPersonViewSet(viewsets.ViewSet):
         serializer = PersonSerializer(board.participants.all(), many=True)
         return Response(serializer.data)
 
+class CardPersonViewSet(viewsets.ViewSet):
+    serializer_class = PersonSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, *args, **kwargs):
+        card = Card.objects.get(id=kwargs['cards_pk'])
+        queryset = card.assigned_to.all()
+        serializer = PersonSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, *args, **kwargs):
+            queryset = Person.objects.filter(associated_list=lists_pk)
+            card = get_object_or_404(queryset, pk=pk)
+            serializer = CardSerializer(card)
+            return Response(serializer.data)
+
+    def create(self, *args, **kwargs):
+
+        board = Board.objects.get(id=kwargs['boards_list_pk'])
+        card = Card.objects.get(id=kwargs['cards_pk'])
+        
+        request_uni = self.request.body.decode('utf-8')
+        body = json.loads(request_uni)
+        person_id = body['id']
+        person = board.participants.get(id=person_id)
+
+        card.assigned_to.add(person)
+
+        serializer = PersonSerializer(card.assigned_to.all(), many=True)
+        return Response(serializer.data)
+
+    def destroy(self, *args, **kwargs):
+        card = Card.objects.get(id=kwargs['cards_pk'])
+        person = Person.objects.get(id=kwargs['pk'])
+        person.delete()
+        serializer = PersonSerializer(board.participants.all(), many=True)
+        return Response(serializer.data)
+
 
         
